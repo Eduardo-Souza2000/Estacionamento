9e6c4465c21580d100524df8de8610390907477e
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-05-20 18:17:26 -0300

    iniciando calculo para tempo com desconto

--- src/main/java/br/com/uniametica/estacionamento/service/MovimentacaoService.java
+++ src/main/java/br/com/uniametica/estacionamento/service/MovimentacaoService.java
@@ -138,63 +138,58 @@ public class MovimentacaoService {
 
     //CHEGAGENS
     if(!movimentacaoRepository.ProcuraId(movimentacao.getId())) {
-            throw new RuntimeException("FAVOR INSERIR UM ID VALIDO");
-    }
-    if(movimentacao.getSaida() == null){
+            throw new RuntimeException("FAVOR INSERIR UMA MOVIMENTAÇÃO VÁLIDA");
+    }else if(movimentacao.getSaida() == null){
             throw new RuntimeException("FAVOR INSERIR A DATA DE SAIDA");
-    }
-    if (movimentacao.getSaida() != null && movimentacao.getEntrada().isAfter(movimentacao.getSaida())){
+    }else if (movimentacao.getSaida() != null && movimentacao.getEntrada().isAfter(movimentacao.getSaida())){
         throw new RuntimeException(" A entrada deve ser antes da saida");
-    }
-    if (movimentacao.getCondutor() == null){
+    }else if (movimentacao.getCondutor() == null){
         throw new RuntimeException("Condutor Nulo");
-    }
-    if(!veiculoRepository.ProcuraId(movimentacao.getVeiculo().getId())){
+    }else if(!veiculoRepository.ProcuraId(movimentacao.getVeiculo().getId())){
         throw new RuntimeException("Veiculo Não Existe No Banco de Dados");
-    }
-    if(!modeloRepository.modeloExistente(movimentacao.getVeiculo().getModelo().getId())){
+    }else if (veiculoRepository.veiculoExistente(movimentacao.getVeiculo().getId()) && movimentacao.getSaida() == null){
+        throw new RuntimeException("Veiculo já está estacionado.");
+    } else if (!veiculoRepository.getById(movimentacao.getVeiculo().getId()).isAtivo()) {
+        throw new RuntimeException("Veiculo inativo");
+    }else if(!modeloRepository.modeloExistente(movimentacao.getVeiculo().getModelo().getId())){
         throw new RuntimeException("Modelo Não Existe No Banco de Dados");
-    }
-    if (!marcaRepository.marcaIdExistentes(movimentacao.getVeiculo().getModelo().getMarca().getId())){
+    }else if (!marcaRepository.marcaIdExistentes(movimentacao.getVeiculo().getModelo().getMarca().getId())){
         throw new RuntimeException("Marca Não Existe No Banco de Dados");
-    }
-    if (marcaRepository.NomeMarcaExistente(String.valueOf(movimentacao.getVeiculo().getModelo().getMarca().getNome().matches("[a-zA-Z]{2,50}")))){
+    } else if (marcaRepository.NomeMarcaExistente(String.valueOf(movimentacao.getVeiculo().getModelo().getMarca().getNome().matches("[a-zA-Z]{2,50}")))){
         throw new RuntimeException("Nome da Marca Invalido");
-    }
-    if(!condutorRepository.idExistente(movimentacao.getCondutor().getId())){
+    } else if(!condutorRepository.idExistente(movimentacao.getCondutor().getId())){
         throw new RuntimeException("Condutor Não Existe No Banco de Dados");
-    }
+    } else if (!condutorRepository.getById(movimentacao.getCondutor().getId()).isAtivo()) {
+        throw new RuntimeException("Condutor inativo.");
+    }else {
 
 
+        //TEMPO DENTRO DO ESTACIONAMENTO
 
-    //TEMPO DENTRO DO ESTACIONAMENTO
+        int tempoMulta = calculaMulta(configuracaoRepository.getById(Long.valueOf(1)), movimentacao);
+        int calculaTempo = calculaTempo(movimentacao);
+        int calculatempoCondutor = calculaTempo(movimentacao);
+        LocalDateTime horaAtual = LocalDateTime.now();
 
-    int tempoMulta = calculaMulta(configuracaoRepository.getById(Long.valueOf(1)),movimentacao);
-    int calculaTempo = calculaTempo(movimentacao);
-    int calculatempoCondutor = calculaTempo(movimentacao);
-    LocalDateTime horaAtual = LocalDateTime.now();
-
-    // if(tempoMulta % 60 != 0)
-    //     tempoMulta+=60;
-
+        // if(tempoMulta % 60 != 0)
+        //     tempoMulta+=60;
 
 
-    movimentacao.setTempoTotalhora(calculaTempo);
-    movimentacao.setTempoTotalminuto(calculaTempo*60);
-    movimentacao.setTempoMultaHora(tempoMulta/60);
-    movimentacao.setTempoMultaMinuto(tempoMulta);
+        movimentacao.setTempoTotalhora(calculaTempo);
+        movimentacao.setTempoTotalminuto(calculaTempo * 60);
+        movimentacao.setTempoMultaHora(tempoMulta / 60);
+        movimentacao.setTempoMultaMinuto(tempoMulta);
 
 
-    movimentacao.setValorHoraMulta(objConfiguracao.getValorMinutoMulta());
-    movimentacao.setValorHora(objConfiguracao.getValorHora());
-    movimentacao.setHoraAtual(horaAtual);
+        movimentacao.setValorHoraMulta(objConfiguracao.getValorMinutoMulta());
+        movimentacao.setValorHora(objConfiguracao.getValorHora());
+        movimentacao.setHoraAtual(horaAtual);
 
 
+        movimentacao.setValorMulta(BigDecimal.valueOf((tempoMulta * objConfiguracao.getValorMinutoMulta().intValue())));
+        movimentacao.setValorTotal(BigDecimal.valueOf((calculaTempo * objConfiguracao.getValorHora().intValue()) + (tempoMulta * objConfiguracao.getValorMinutoMulta().intValue())));
 
-    movimentacao.setValorMulta(BigDecimal.valueOf((tempoMulta * objConfiguracao.getValorMinutoMulta().intValue())));
-    movimentacao.setValorTotal(BigDecimal.valueOf((calculaTempo * objConfiguracao.getValorHora().intValue()) +  (tempoMulta * objConfiguracao.getValorMinutoMulta().intValue())));
-
-    //(calculaTempo * objConfiguracao.getValorHora().intValue())
+        //(calculaTempo * objConfiguracao.getValorHora().intValue())
 
 //    calculatempoCondutor =+ calculatempoCondutor;
 //    movimentacao.getCondutor().setTempototal(calculatempoCondutor);
@@ -209,50 +204,54 @@ public class MovimentacaoService {
 //    movimentacao.setValorTotal(BigDecimal.valueOf(movimentacao.getTempoTotalhora() + valorHora.intValue()).add(BigDecimal.valueOf(movimentacao.getTempoMultaMinuto() + valorMinutoMulta.intValue())));
 
 
+        //TEMPO DO CONDUTOR DO ESTACIONAMENTO
+
+        //CONVERTE UM VALOR LONG EM INT
+        int condutorExistente = Math.toIntExact(movimentacao.getCondutor().getId());
+
+        //PESQUISA O NUMERO DO ID CONFORME FOI PASSADO ANTERIORMENTE
+        Condutor condutorBanco = condutorRepository.getById((long) condutorExistente);
+
+        //CALCULO DO TEMPO ELE PUXA O TEMPO TOTAL DO BANCO + CALCULO DO RETORNO DE CALCULA TEMPO CONDUTOR
+        int tempoNovo = condutorBanco.getTempototal() + calculatempoCondutor;
+
+        //CALCULO DO TEMPO ELE PUXA O TEMPO TOTAL DO BANCO + CALCULO DO RETORNO DE CALCULA TEMPO CONDUTOR
+        int tempoNovoPago = condutorBanco.getTempoPago() + calculatempoCondutor;
+
+        condutorBanco.setTempototal(tempoNovo);
+
+        condutorBanco.setTempoPago(tempoNovoPago);
+
+        movimentacao.getCondutor().setTempototal(condutorBanco.getTempototal());
 
 
+        //TEMPO DE DESCONTO
 
-                                     //TEMPO DO CONDUTOR DO ESTACIONAMENTO
+        if (condutorBanco.getTempoPago() > 50) {
+            //CALCULA O VALOR DO DESCONTO
+            condutorBanco.setTempoDesconto((condutorBanco.getTempoPago() / 50 * 5) + condutorBanco.getTempoDesconto());
 
-    //CONVERTE UM VALOR LONG EM INT
-    int condutorExistente = Math.toIntExact(movimentacao.getCondutor().getId());
+            //CALCULA O TANTO QUE SOBROU DO CONDUTOR PARA DEIXAR ARMAZENADO PARA AS PROXIMAS 50 HORAS PARA DESCONTO
+            condutorBanco.setTempoPago(condutorBanco.getTempoPago() % 50);
 
-    //PESQUISA O NUMERO DO ID CONFORME FOI PASSADO ANTERIORMENTE
-    Condutor condutorBanco = condutorRepository.getById((long) condutorExistente);
+           // int valorTotalDesconto = calculaTempoComDesconto(movimentacao,configuracao);
 
-    //CALCULO DO TEMPO ELE PUXA O TEMPO TOTAL DO BANCO + CALCULO DO RETORNO DE CALCULA TEMPO CONDUTOR
-    int tempoNovo = condutorBanco.getTempototal() + calculatempoCondutor;
+            movimentacao.setValorTotal(BigDecimal.valueOf((calculaTempo * objConfiguracao.getValorHora().intValue()) + (tempoMulta * objConfiguracao.getValorMinutoMulta().intValue()) ));//- condutorBanco.getTempoDesconto()));
 
-    //CALCULO DO TEMPO ELE PUXA O TEMPO TOTAL DO BANCO + CALCULO DO RETORNO DE CALCULA TEMPO CONDUTOR
-    int tempoNovoPago = condutorBanco.getTempoPago() + calculatempoCondutor;
+        }
 
-    condutorBanco.setTempototal(tempoNovo);
-
-    condutorBanco.setTempoPago(tempoNovoPago);
-
-    movimentacao.getCondutor().setTempototal(condutorBanco.getTempototal());
+        condutorRepository.save(condutorBanco);
 
 
-                                    //TEMPO DE DESCONTO
+        movimentacao.getCondutor().setTempoDesconto(condutorBanco.getTempoDesconto());
+        movimentacao.getCondutor().setTempoPago(condutorBanco.getTempoPago());
+        movimentacao.setTempoDesconto(condutorBanco.getTempoDesconto());
 
-    if (condutorBanco.getTempoPago() > 50){
-    //CALCULA O VALOR DO DESCONTO
-    condutorBanco.setTempoDesconto((condutorBanco.getTempoPago()/50*5) + condutorBanco.getTempoDesconto());
 
-    //CALCULA O TANTO QUE SOBROU DO CONDUTOR PARA DEIXAR ARMAZENADO PARA AS PROXIMAS 50 HORAS PARA DESCONTO
-    condutorBanco.setTempoPago(condutorBanco.getTempoPago() % 50);
+        movimentacaoRepository.save(movimentacao);
 
 
     }
-
-    condutorRepository.save(condutorBanco);
-
-    movimentacao.getCondutor().setTempoDesconto(condutorBanco.getTempoDesconto());
-    movimentacao.getCondutor().setTempoPago(condutorBanco.getTempoPago());
-
-    movimentacaoRepository.save(movimentacao);
-
-
 
     return movimentacao.toString();
 }
@@ -318,6 +317,38 @@ public class MovimentacaoService {
     }
 
 
+public int calculaTempoComDesconto(Movimentacao movimentacao, Configuracao configuracao){
 
+    LocalDateTime entrada = movimentacao.getEntrada();
+    LocalDateTime saida = movimentacao.getSaida();
+    LocalTime inicioExpediente = configuracao.getInicioExpediente();
+    LocalTime fimExpediente = configuracao.getFimExpediente();
+    int tempoDesconto = 0;
+    int AnoEntrada = entrada.getYear();
+    int saidaAno = saida.getYear();
+    int totalDias = 0;
+
+    if(AnoEntrada != saidaAno){
+        totalDias += saidaAno - AnoEntrada;
+    } else{
+        totalDias += saida.getDayOfYear() - entrada.getDayOfYear();
+    }
+    if (  inicioExpediente.isAfter(entrada.toLocalTime())){
+        tempoDesconto += ((int) Duration.between(entrada.toLocalTime(), inicioExpediente).toMinutes());
+    }
+    if (fimExpediente.isBefore(saida.toLocalTime()))
+    {
+        tempoDesconto += ((int) Duration.between(fimExpediente, saida.toLocalTime()).toMinutes());
+
+    }
+    if (totalDias > 0){
+        int diferenca = ((int) Duration.between(inicioExpediente, fimExpediente).toMinutes());//getSeconds()/60);
+        tempoDesconto +=   (totalDias * 24 * 60 ) - (diferenca + movimentacao.getCondutor().getTempoDesconto());
+
+    }
+
+
+    return tempoDesconto;
+}
 
 }
